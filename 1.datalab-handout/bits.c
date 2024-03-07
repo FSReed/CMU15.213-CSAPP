fa97cbfb2016774d88e9c2c6b5a6d55c5cb9bf23
FSReed <1714747596@qq.com>
2024-03-07 10:48:01 +0800

    logicalNeg passed

--- 1.datalab-handout/bits.c
+++ 1.datalab-handout/bits.c
@@ -252,7 +252,13 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+	/* if Neg, then x is not 0
+	 * if Pos, then -x should be neg!!!
+	*/
+	int neg = (x >> 31) & 1;
+	int pos = ((~x + 1) >> 31) & 1;
+	int label = ~(neg | pos) + 1;		// if neg or pos, label = -1
+	return label + 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
