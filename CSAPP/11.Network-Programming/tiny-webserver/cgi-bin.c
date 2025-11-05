7bfd396e139711ce8aee7d29fe6f3203c680ec96
FSReed <1714747596@qq.com>
2025-11-05 19:38:38 +0800

    feat(CSAPP): Homework 11.10

--- CSAPP/11.Network-Programming/tiny-webserver/cgi-bin.c
+++ CSAPP/11.Network-Programming/tiny-webserver/cgi-bin.c
@@ -3,16 +3,23 @@
 #include <stdlib.h>
 
 int main(void) {
+  char content[64];
   char *args = getenv("QUERY_STRING");
   char *p = strstr(args, "&");
   if (!p) {
     printf("Need 2 arguments\n");
     return -1;
   }
+  int arg1 = 0, arg2 = 0;
+  sscanf(args, "first=%d&second=%d", &arg1, &arg2);
+  sprintf(content, "Answer: %d + %d = %d\r\n", arg1, arg2, arg1 + arg2);
 
-  *p = '\0';
-  int arg1 = atoi(args);
-  int arg2 = atoi(p + 1);
-  printf("Answer: %d\n", arg1 + arg2);
+  printf("Connection close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n");
+  printf("\r\n");
+
+  printf("%s", content);
+  fflush(stdout);
   return 0;
 }
